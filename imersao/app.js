ca502a2d7199c227cf56fdd5bc00896a5c48e6d6
Gustavo Simão | GS.dev <gs14.dev@gmail.com>
2024-09-05 18:55:14 -0300

    Aula 4 continuação
    Definição da função do botão pesquisar, comentário com Gemini, e afinação do código

--- imersao/app.js
+++ imersao/app.js
@@ -1,23 +1,27 @@
 // comentário"//"
-
-let section = document.getElementById("resultados-pesquisa")
-console.log(section);
-
-// Dica da Ballerine
-// Portugues = Para cada (for) dado dentro da lista de dados (let var)
-// Javascript = for (let dado of dados)
-
-for (let dado of dados) {
+// Dica da Ballerine 
+// Portugues = Para cada (for) dado dentro da lista de dados (let var) 
+// Javascript = for (let dado of dados) 
 // += é uma forma abreviada de realizar uma operação de adição e atribuição simultaneamente. Em outras palavras, ele adiciona um valor a uma variável existente e, em seguida, atribui o resultado dessa adição à mesma variável. (Incremento, concatenação)
-// 
-    section.innerHTML += `
-        <div class="item-resultado">
 
-            <h2>
-                <a href="#" target="_blank">
-                    ${dado.titulo}
-                <a>                 
-            </h2>
+function pesquisar() {
+
+    // Seleciona a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa")
+
+    // Inicializa uma string vazia para concatenar os resultados da pesquisa
+    let resultados = "";
+
+    // Itera sobre cada dado no array 'dados'
+    for (let dado of dados) {
+        resultados +=
+`           <div class="item-resultado">
+
+                <h2>
+                    <a href="#" target="_blank">
+                        ${dado.titulo}
+                    <a>                 
+                </h2>
 
                 <p class="descricao-meta">
                     ${dado.descricao}
@@ -27,9 +31,9 @@ for (let dado of dados) {
                     Ver mais...
                 </a>
 
-    </div>
-`
-}
+            </div>  `
+    };
 
-// Voltar a partir da Aula 4 - 25:21 min
-
+// Atribui o HTML gerado para a seção de resultados    
+section.innerHTML = resultados
+};
