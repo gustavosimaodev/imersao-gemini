0ce592983f95c5ffca5b1f24af0b3736a3696b92
Gustavo Simão | GS.dev <gs14.dev@gmail.com>
2024-09-05 09:56:54 -0300

    Aula 4
    Definiçao de parametros dentro do JS
    Js escrevendo dentro do HTML
    Importação das informações do HTML para o JS devolver dentro do HTML

--- imersao/app.js
+++ imersao/app.js
@@ -1,1 +1,35 @@
-console.log(dados[0].titulo);
+// comentário"//"
+
+let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+// Dica da Ballerine
+// Portugues = Para cada (for) dado dentro da lista de dados (let var)
+// Javascript = for (let dado of dados)
+
+for (let dado of dados) {
+// += é uma forma abreviada de realizar uma operação de adição e atribuição simultaneamente. Em outras palavras, ele adiciona um valor a uma variável existente e, em seguida, atribui o resultado dessa adição à mesma variável. (Incremento, concatenação)
+// 
+    section.innerHTML += `
+        <div class="item-resultado">
+
+            <h2>
+                <a href="#" target="_blank">
+                    ${dado.titulo}
+                <a>                 
+            </h2>
+
+                <p class="descricao-meta">
+                    ${dado.descricao}
+                </p>
+
+                <a href=${dado.link} target="_blank">
+                    Ver mais...
+                </a>
+
+    </div>
+`
+}
+
+// Voltar a partir da Aula 4 - 25:21 min
+
--- imersao/dados.js
+++ imersao/dados.js
@@ -2,32 +2,32 @@ let dados = [
     {
         titulo : "Identificação correta do paciente",
         descricao : "Essa meta enfatiza a importância de confirmar a identidade do paciente em todas as etapas do atendimento, evitando erros como cirurgias em pacientes errados.",
-        link : "#"
+        link : "Ver mais..."
     },
     {
         titulo: "Comunicação efetiva",
         descricao: "A comunicação clara e precisa entre os profissionais de saúde é fundamental para evitar mal entendidos e garantir que as informações corretas sejam transmitidas.",
-        link: "#"
+        link: "Ver mais..."
     },
     {
         titulo: "Melhorar a segurança dos medicamentos",
         descricao: "Essa meta abrange desde a prescrição até a administração de medicamentos, visando reduzir erros de medicação que podem causar danos ao paciente.",
-        link: "#"
+        link: "Ver mais..."
     },
     {
         titulo: "Cirurgia segura",
         descricao: "A cirurgia segura envolve a confirmação do paciente, do procedimento e do local da cirurgia, além de outras medidas para garantir a segurança durante o ato cirúrgico.",
-        link: "#"
+        link: "Ver mais..."
     },
     {
         titulo: "Reduzir o risco de infecções associadas aos cuidados de saúde",
         descricao: "Essa meta busca prevenir infecções hospitalares, que podem complicar o tratamento e aumentar a mortalidade.",
-        link: "#"
+        link: "Ver mais..."
     },
     {
         titulo: "Reduzir o risco de danos ao paciente, decorrente de quedas",
         descricao: "As quedas são um dos eventos adversos mais comuns em hospitais e podem causar lesões graves aos pacientes. Essa meta busca implementar medidas para prevenir quedas.",
-        link: "#"
+        link: "Ver mais..."
     }
 ];
 
